6291b7c9f627bde9092d8b499050285bbe545c2f
Randall Fowler <rafowler@randactyl.com>
2015-03-06 13:46:18 -0800

    initial commit. Addon version 1.0.0.0
    Hides items that are bound to your character/account from the guild bank
    deposit scene.

---
+++ HideBoundItems.lua
@@ -0,0 +1,16 @@
+local libFilters = LibStub("libFilters")
+
+local function HideBoundItems()
+	local function filterCallback(slot)
+		return not IsItemBound(slot.bagId, slot.slotIndex)
+	end
+
+	libFilters:RegisterFilter("HideBoundItems", LAF_BAGS, filterCallback)
+end
+
+local function UnhideBoundItems()
+	libFilters:UnregisterFilter("HideBoundItems", LAF_BAGS)
+end
+
+EVENT_MANAGER:RegisterForEvent("HideBoundItems", EVENT_OPEN_GUILD_BANK, HideBoundItems)
+EVENT_MANAGER:RegisterForEvent("UnhideBoundItems", EVENT_CLOSE_GUILD_BANK, UnhideBoundItems)

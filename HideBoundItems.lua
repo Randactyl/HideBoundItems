c78ccffa8f2e143943e892ab5fb9f15e183d5273
Randactyl <rafowler@randactyl.com>
2017-01-25 12:47:42 -0800

    whitespace cleanup

--- HideBoundItems.lua
+++ HideBoundItems.lua
@@ -2,23 +2,23 @@ local LibFilters = LibStub("LibFilters-2.0")
 LibFilters:InitializeLibFilters()
 
 local function filterCallback(slot)
-	return not IsItemBound(slot.bagId, slot.slotIndex)
+    return not IsItemBound(slot.bagId, slot.slotIndex)
 end
 
 local function HideBoundItemsAtBank()
-	LibFilters:RegisterFilter("HideBoundItems", LF_GUILDBANK_DEPOSIT, filterCallback)
-	LibFilters:RequestUpdate(LF_GUILDBANK_DEPOSIT)
+    LibFilters:RegisterFilter("HideBoundItems", LF_GUILDBANK_DEPOSIT, filterCallback)
+    LibFilters:RequestUpdate(LF_GUILDBANK_DEPOSIT)
 end
 
 local function HideBoundItemsAtAuction()
-	LibFilters:RegisterFilter("HideBoundItems", LF_GUILDSTORE_SELL, filterCallback)
-	LibFilters:RequestUpdate(LF_GUILDSTORE_SELL)
+    LibFilters:RegisterFilter("HideBoundItems", LF_GUILDSTORE_SELL, filterCallback)
+    LibFilters:RequestUpdate(LF_GUILDSTORE_SELL)
 end
 
 local function UnhideBoundItems()
-	LibFilters:UnregisterFilter("HideBoundItems")
-	LibFilters:RequestUpdate(LF_GUILDBANK_DEPOSIT)
-	LibFilters:RequestUpdate(LF_GUILDSTORE_SELL)
+    LibFilters:UnregisterFilter("HideBoundItems")
+    LibFilters:RequestUpdate(LF_GUILDBANK_DEPOSIT)
+    LibFilters:RequestUpdate(LF_GUILDSTORE_SELL)
 end
 
 EVENT_MANAGER:RegisterForEvent("HideBoundItems", EVENT_OPEN_GUILD_BANK, HideBoundItemsAtBank)
